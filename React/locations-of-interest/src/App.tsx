f37aa0bc8a3c2730419d97053c9ca1e2bb76e020
Julian Adler <julian.a@passport.com>
2021-08-23 16:30:07 +1200

    Delay on input

--- React/locations-of-interest/src/App.tsx
+++ React/locations-of-interest/src/App.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import LocationTable from "./Component/LocationTable";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Input } from "@material-ui/core";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [finishedSearch, setDisplayMessage] = useState("");
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => setDisplayMessage(search), 500);
+    return () => clearTimeout(timeOutId);
+  }, [search]);
 
   return (
     <React.Fragment>
@@ -13,7 +19,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setSearch(e.currentTarget.value)}
       />
-      <LocationTable search={search} />
+      <LocationTable search={finishedSearch} />
     </React.Fragment>
   );
 }
